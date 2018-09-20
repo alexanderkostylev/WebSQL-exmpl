135806b61f3bfd46bcce0ec16c7bb388fea27914
alexanderkostylev <alexander.rels01@yandex.com>
2018-09-20 21:58:28 +0500

    small fix

--- src/js/code.js
+++ src/js/code.js
@@ -1,6 +1,7 @@
 "use strict";
 
 let db;
+const initialArray = ['....╚⊙ ⊙╝....','.╚═(███)═╝','..╚═(███)═╝','...╚═(███)═╝','..╚═(███)═╝'];
 
 function initDb() {
   db = openDatabase('list', '1.0', 'list', 1 * 1024 * 1024);
@@ -29,17 +30,27 @@ function renderOutput() {
       console.log(result.rows);
       result.rows.forEach = [].forEach;
       result.rows.forEach((item, index) => {
-        output.innerHTML += `${(index+1)}) ${item.label} </br>`;
+        output.innerHTML += `${(item.id)}) ${item.label} </br>`;
       });
     })
   });
 }
 
+function initialState() {
+  db.transaction((tx) => {
+    tx.executeSql('DELETE FROM list');
+    initialArray.forEach((item) => {
+      tx.executeSql('INSERT INTO list (label) values (?)', [item]);
+    });
+    renderOutput();
+  });
+}
 function main() {
   const textArea = document.querySelector('.input__content__textarea');
   const submitButton = document.querySelector('.input__content__button');
   initDb();
   createTable();
+  initialState();
   submitButton.onclick = () => addСommand(String(textArea.value));
 }
 
